ee927b2441624592f6788c22d0a66a420285f852
Toberumono <JoshuaLipstone@gmail.com>
2015-07-23 14:19:46 -0400

    Another trial

--- lexer.rb
+++ lexer.rb
@@ -1,24 +1,18 @@
 class Lexer < Formula
 	desc "A library for lexing text, similar to flex for C/C++."
 	homepage "https://github.com/Toberumono/Lexer"
-	revision 1
+	revision 3
 
 	url "https://github.com/Toberumono/Lexer.git", :using => :git, :tag => "Stable"
 
 	version "2.1"
 
-	option "with-prefix", "The root directory in which to download and compile the library [Default: ~/libraries/]"
+	#option "prefix", "The root directory in which to download and compile the library [Default: ~/libraries/]"
 
 	depends_on "ant" => :build
 
 	def install
-		opoo "with-prefix = #{with-prefix}"
-		args = []
-		if build.with? "prefix"
-			args << "--Dprefix=#{with-prefix}"
-		else
-			args << "--Dprefix=~/libraries/"
-		end
+		args << "--Dprefix=./"
 		system "ant", *args
 	end
 end
