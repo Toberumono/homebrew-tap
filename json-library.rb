5eb0f726ec4a81a8ba287b76c2be4c059d58db2c
Toberumono <JoshuaLipstone@gmail.com>
2015-07-23 18:09:36 -0400

    Adding the JSON library

---
+++ json-library.rb
@@ -0,0 +1,22 @@
+class JSONLibrary < Formula
+	desc "A library for parsing JSON text and reading and writing said text to and from files."
+	homepage "https://github.com/Toberumono/JSON-Library"
+	revision 0
+
+	url "https://github.com/Toberumono/JSON-Library.git", :using => :git
+
+	version "2.0"
+
+	#option "prefix", "The root directory in which to download and compile the library [Default: ~/libraries/]"
+
+	depends_on "ant" => :build
+
+	def install
+		system "ant", "-Dprefix=\"./\"", "-Dlibs=\"#{HOMEBREW_PREFIX}/lib\""
+		lib.install "JSONLib.jar"
+	end
+
+	def caveats
+		"In order to use this library in a Java program, add #{HOMEBREW_PREFIX}/lib to your classpath."
+	end
+end
