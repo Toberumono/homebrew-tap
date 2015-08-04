c716d3f3b30d9619d24983f4aa5b3c3f8ea56383
Toberumono <JoshuaLipstone@gmail.com>
2015-08-04 11:33:34 -0400

    Versioning

--- wrf-runner.rb
+++ wrf-runner.rb
@@ -4,7 +4,7 @@ class WrfRunner < Formula
   @@project_url="https://github.com/Toberumono/WRF-Runner"
   homepage "#{@@project_url}"
 
-  url "#{@@project_url}.git", :tag => "1.5.2a"
+  url "#{@@project_url}.git", :tag => "1.5.3"
 
   head "#{@@project_url}.git"
 
