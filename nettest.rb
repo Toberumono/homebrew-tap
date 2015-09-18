6b86394d439ac5986aa04bd6dea89d9461f9077c
Toberumono <JoshuaLipstone@gmail.com>
2015-09-18 15:04:00 -0500

    Versioning

--- nettest.rb
+++ nettest.rb
@@ -2,13 +2,14 @@ class Nettest < Formula
   desc "A utility for quickly checking a computer's Internet connection status targeted at lazy people."
   @@project_url="https://github.com/Toberumono/Miscellaneous"
   homepage "#{@@project_url}/tree/master/common"
+  revision 1
 
   url "#{@@project_url}.git", :tag => "1.6.0"
 
   head "#{@@project_url}.git"
 
   def install
-    bin.install "nettest"
+    bin.install "common/nettest" => "nettest"
   end
 
   def caveats
