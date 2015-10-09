caa430de8ca5cde21d073aa81948f0c31d2ef340
Toberumono <JoshuaLipstone@gmail.com>
2015-10-09 12:15:12 -0500

    Versioning

--- lexer.rb
+++ lexer.rb
@@ -4,7 +4,7 @@ class Lexer < Formula
   @@project_url="https://github.com/Toberumono/Lexer"
   homepage "#{@@project_url}"
 
-  url "#{@@project_url}.git", :tag => "3.6.4"
+  url "#{@@project_url}.git", :tag => "3.6.5"
 
   head "#{@@project_url}.git"
 
