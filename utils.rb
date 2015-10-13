8a7a7050d02f13b11253f6fbcbed85ae99d46d8b
Toberumono <JoshuaLipstone@gmail.com>
2015-10-13 14:29:07 -0500

    Versioning

--- utils.rb
+++ utils.rb
@@ -4,7 +4,7 @@ class Utils < Formula
   @@project_url="https://github.com/Toberumono/Utils"
   homepage "#{@@project_url}"
 
-  url "#{@@project_url}.git", :tag => "1.7.2"
+  url "#{@@project_url}.git", :tag => "1.7.3"
 
   head "#{@@project_url}.git"
 
