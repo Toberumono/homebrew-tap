ca340f71202b515eb1d45bba86cb5ae2de9688e4
Toberumono <JoshuaLipstone@gmail.com>
2015-10-17 18:39:43 -0500

    Versioning

--- namelist-parser.rb
+++ namelist-parser.rb
@@ -4,7 +4,7 @@ class NamelistParser < Formula
   @@project_url="https://github.com/Toberumono/Namelist-Parser"
   homepage "#{@@project_url}"
 
-  url "#{@@project_url}.git", :tag => "2.3.1"
+  url "#{@@project_url}.git", :tag => "2.3.2"
 
   head "#{@@project_url}.git"
 
