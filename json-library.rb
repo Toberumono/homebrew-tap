504d8935eddf146a23a0153c5940bcbd11c1c790
Toberumono <JoshuaLipstone@gmail.com>
2016-06-13 18:20:36 -0500

    Versioning

--- json-library.rb
+++ json-library.rb
@@ -4,7 +4,7 @@ class JsonLibrary < Formula
   @@project_url="https://github.com/Toberumono/JSON-Library"
   homepage "#{@@project_url}"
 
-  url "#{@@project_url}.git", :tag => "3.4.0"
+  url "#{@@project_url}.git", :tag => "3.4.1"
 
   head "#{@@project_url}.git"
 
